917125035028d3ae71fc034351e8e3b29d34f5d3
Sora-Ishi <t18050si@sfc.keio.ac.jp>
2019-01-14 10:24:21 +0900

    コミット

---
+++ project.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".START").click(function(){
+        $(this).fadeOut(2000)
+    });
+});
+
+$(function(){
+    $(".aaa").click(function(){
+        $(this).fadeOut(2000)
+    });
+});
