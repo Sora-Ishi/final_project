2e03963d83a778a03361c1354b289fa24aeaa342
Sora-Ishi <t18050si@sfc.keio.ac.jp>
2019-01-14 13:09:38 +0900

    コミット

--- project.js
+++ project.js
@@ -1,3 +1,13 @@
+function effect() {
+var element = document.getElementById("toukou");
+element.style.backgroundImage = 'url(https://sora-ishi.github.io/final_project/illustration/bg_school_yuyake.jpg)';
+}
+
+function effect() {
+var element = document.getElementById("furyou");
+element.style.backgroundImage = 'url(https://sora-ishi.github.io/final_project/illustration/furyo_shounen-1.png)';
+}
+
 $(function(){
     $(".START").click(function(){
         $(this).fadeOut(2000)
